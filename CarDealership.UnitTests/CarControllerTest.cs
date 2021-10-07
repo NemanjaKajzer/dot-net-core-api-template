74c1e3caaae4356ee380b6bda808829444ccd5f3
Nemanja Kajzer <n.kajzer@vega.local>
2021-10-07 13:12:36 +0200

    Implemented tests for all methods Added invalid request testing

--- CarDealership.UnitTests/CarControllerTest.cs
+++ CarDealership.UnitTests/CarControllerTest.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Threading.Tasks;
 using CarDealership.API.Common.Response;
 using Xunit;
 
@@ -49,13 +50,116 @@ namespace CarDealership.UnitTests
 
             mockCarService.Setup(carService => carService.AddCarAsync(expectedDTO)).ReturnsAsync(new Car { Id = carId });
             mockMapper.Setup(mapper => mapper.Map<CarDTO>(It.IsAny<Car>())).Returns(expectedDTO);
+            mockResponse.Setup(response =>
+                response.CustomStatusCode(500, new Exception("Object reference not set to an instance of an object.")));
 
             var controller = new CarController(mockCarService.Object, mockMapper.Object, mockLogger.Object, mockResponse.Object);
 
-            var result = controller.AddCarAsync(expectedDTO).Result as OkObjectResult; ;
+            var result = controller.AddCarAsync(expectedDTO).Result as OkObjectResult;
 
             Assert.NotNull(result);
             Assert.Equal(200, result.StatusCode);
+        }
+
+        [Fact]
+        public void UpdateCarAsyncTest()
+        {
+            var carId = Guid.NewGuid();
+            var inputDTO = new CarDTO { Id = carId, Brand = "Changed"};
+            var notValidDTO = new CarDTO { Brand = "Changed" };
+
+            var mockCarService = new Mock<ICarService>();
+            var mockMapper = new Mock<IMapper>();
+            var mockLogger = new Mock<ILogger<CarController>>();
+            var mockResponse = new Mock<IResponseStatus>();
+
+            mockCarService.Setup(carService => carService.UpdateCarAsync(inputDTO)).ReturnsAsync(new Car { Id = carId });
+            mockMapper.Setup(mapper => mapper.Map<CarDTO>(It.IsAny<Car>())).Returns(inputDTO);
+            mockResponse.Setup(response =>
+                response.CustomStatusCode(500, It.IsAny<Exception>())).Returns(new StatusCodeResult(500));
+
+            var controller = new CarController(mockCarService.Object, mockMapper.Object, mockLogger.Object, mockResponse.Object);
+
+            var result = controller.UpdateCarAsync(inputDTO).Result as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+        }
+
+        [Fact]
+        public void UpdateCarInvalidAsyncTest()
+        {
+            var carId = Guid.NewGuid();
+            var notValidDTO = new CarDTO { Brand = "Changed" };
+
+            var mockCarService = new Mock<ICarService>();
+            var mockMapper = new Mock<IMapper>();
+            var mockLogger = new Mock<ILogger<CarController>>();
+            var mockResponse = new Mock<IResponseStatus>();
+
+            mockCarService.Setup(carService => carService.UpdateCarAsync(notValidDTO)).ThrowsAsync(new Exception("Object reference not set to an instance of an object."));
+            mockMapper.Setup(mapper => mapper.Map<CarDTO>(It.IsAny<Car>())).Returns(notValidDTO);
+            mockResponse.Setup(response =>
+                response.CustomStatusCode(500, It.IsAny<Exception>())).Returns(new StatusCodeResult(500));
+
+            var controller = new CarController(mockCarService.Object, mockMapper.Object, mockLogger.Object, mockResponse.Object);
+
+            var result = controller.UpdateCarAsync(notValidDTO).Result as StatusCodeResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(500, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteCarAsyncTest()
+        {
+            var carId = Guid.NewGuid();
+            var expectedDTO = new CarDTO { Id = carId };
+            var car = new Car {Id = carId};
+
+            var mockCarService = new Mock<ICarService>();
+            var mockMapper = new Mock<IMapper>();
+            var mockLogger = new Mock<ILogger<CarController>>();
+            var mockResponse = new Mock<IResponseStatus>();
+
+            mockCarService.Setup(carService => carService.GetCarByIdAsync(carId)).ReturnsAsync(car);
+            mockCarService.Setup(carService => carService.DeleteCarAsync(It.IsAny<Guid>())).ReturnsAsync(car);
+
+            mockMapper.Setup(mapper => mapper.Map<CarDTO>(It.IsAny<Car>())).Returns(expectedDTO);
+            mockResponse.Setup(response =>
+                response.CustomStatusCode(500, It.IsAny<Exception>())).Returns(new StatusCodeResult(500));
+
+            var controller = new CarController(mockCarService.Object, mockMapper.Object, mockLogger.Object, mockResponse.Object);
+
+            var result = controller.DeleteCarAsync(carId).Result as StatusCodeResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteCarInvalidAsyncTest()
+        {
+            var invalidId = Guid.NewGuid();
+            var expectedDTO = new CarDTO { Id = invalidId };
+
+            var mockCarService = new Mock<ICarService>();
+            var mockMapper = new Mock<IMapper>();
+            var mockLogger = new Mock<ILogger<CarController>>();
+            var mockResponse = new Mock<IResponseStatus>();
+
+            mockCarService.Setup(carService => carService.DeleteCarAsync(invalidId)).ThrowsAsync(new Exception("Object reference not set to an instance of an object."));
+
+            mockMapper.Setup(mapper => mapper.Map<CarDTO>(It.IsAny<Car>())).Returns(expectedDTO);
+            mockResponse.Setup(response =>
+                response.CustomStatusCode(500, It.IsAny<Exception>())).Returns(new StatusCodeResult(500));
+
+            var controller = new CarController(mockCarService.Object, mockMapper.Object, mockLogger.Object, mockResponse.Object);
+
+            var result = controller.DeleteCarAsync(invalidId).Result as StatusCodeResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(500, result.StatusCode);
         }
     }
 }
